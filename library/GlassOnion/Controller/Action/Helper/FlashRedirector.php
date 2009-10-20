d6037387fd0def8a275069eb56641cbce9fff140
Cizar <cesarkastli@gmail.com>
2009-10-20 21:44:49 -0200

    Usar Zym_Controller_Action_Helper_FlashRedirector

--- library/GlassOnion/Controller/Action/Helper/FlashRedirector.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'Zend/Controller/Action/Helper/Abstract.php';
-
-class GlassOnion_Controller_Action_Helper_FlashRedirector extends Zend_Controller_Action_Helper_Abstract
-{
-	// TODO
-}
