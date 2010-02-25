b5d432da298ac7ed4a776d4517d6befa4673fc30
Cesar Kastli <ckastli@debian.(none)>
2010-02-25 18:39:07 -0200

    Fix

--- library/GlassOnion/Application/Resource/Doctrine.php
+++ library/GlassOnion/Application/Resource/Doctrine.php
@@ -9,37 +9,37 @@ class GlassOnion_Application_Resource_Doctrine extends  Zend_Application_Resourc
      */
     public function init()
     {
-	$config = $this->getOptions();
+        $config = $this->getOptions();
 
-	require_once 'Doctrine.php';
+        require_once 'Doctrine.php';
 
-	Zend_Loader_Autoloader::getInstance()
-		->pushAutoloader(array('Doctrine_Core', 'autoload'), 'Doctrine');
+        Zend_Loader_Autoloader::getInstance()
+                ->pushAutoloader(array('Doctrine_Core', 'autoload'));
 
-	$manager = Doctrine_Manager::getInstance();
+        $manager = Doctrine_Manager::getInstance();
 
-	$manager->setAttribute(
-		Doctrine::ATTR_MODEL_LOADING, 
-		Doctrine::MODEL_LOADING_CONSERVATIVE);
+        $manager->setAttribute(
+                Doctrine::ATTR_MODEL_LOADING,
+                Doctrine::MODEL_LOADING_CONSERVATIVE);
 
-	$manager->setAttribute(
-		Doctrine_Core::ATTR_AUTOLOAD_TABLE_CLASSES,
-		TRUE);
+        $manager->setAttribute(
+                Doctrine_Core::ATTR_AUTOLOAD_TABLE_CLASSES,
+                TRUE);
 
-	$manager->setAttribute(
-		Doctrine::ATTR_VALIDATE,
-		Doctrine::VALIDATE_ALL);
+        $manager->setAttribute(
+                Doctrine::ATTR_VALIDATE,
+                Doctrine::VALIDATE_ALL);
 
-	$manager->setAttribute(
-		Doctrine_Core::ATTR_USE_DQL_CALLBACKS,
-		TRUE);
+        $manager->setAttribute(
+                Doctrine_Core::ATTR_USE_DQL_CALLBACKS,
+                TRUE);
 
-	$manager->setAttribute(
-		Doctrine::ATTR_QUOTE_IDENTIFIER,
-		TRUE);
+        $manager->setAttribute(
+                Doctrine::ATTR_QUOTE_IDENTIFIER,
+                TRUE);
 
-	$manager->openConnection($config['connection_string']);
+        $manager->openConnection($config['connection_string']);
 
-	return $manager;
+        return $manager;
     }
 }
