c94df89a9420d32311e6e13023c2a8a1956c33e7
cizar <cesarkastli@gmail.com>
2012-07-28 20:27:30 -0300

    Fix ActionHelpers

--- library/GlassOnion/Controller/Action/Helper/AuthIdentity.php
+++ library/GlassOnion/Controller/Action/Helper/AuthIdentity.php
@@ -8,6 +8,7 @@ require_once 'Zend/Controller/Action/Helper/Abstract.php';
 /**
  * @category   GlassOnion
  * @package    GlassOnion_Controller
+ * @subpackage Helper
  */
 class GlassOnion_Controller_Action_Helper_AuthIdentity
     extends Zend_Controller_Action_Helper_Abstract
@@ -17,11 +18,10 @@ class GlassOnion_Controller_Action_Helper_AuthIdentity
      *
      * @return mixed
      */
-	public function direct($field = null)
-	{
-		$identity = Zend_Auth::getInstance()
-			->getIdentity();
+    public function direct($field = null)
+    {
+        $identity = Zend_Auth::getInstance()->getIdentity();
 
-		return is_null($field) ? $identity : $identity->$field;
-	}
+        return is_null($field) ? $identity : $identity->$field;
+    }
 }
--- library/GlassOnion/Controller/Action/Helper/Log.php
+++ library/GlassOnion/Controller/Action/Helper/Log.php
@@ -10,14 +10,55 @@ require_once 'Zend/Controller/Action/Helper/Abstract.php';
  * @package    GlassOnion_Controller
  * @subpackage Helper
  */
-class GlassOnion_Controller_Action_Helper_Log extends Zend_Controller_Action_Helper_Abstract
+class GlassOnion_Controller_Action_Helper_Log
+    extends Zend_Controller_Action_Helper_Abstract
 {
+    /**
+     * @var Zend_Log
+     */
+    private $_logger = null;
+    
 	/**
-	 * @return void
+     * Returns the logger and logs a message if it's defined
+     *
+	 * @return Zend_Log
 	 */
-	public function direct($message, $priority = Zend_Log::INFO)
+	public function direct($message = null, $priority = Zend_Log::INFO)
 	{
-		$bootstrap = $this->getActionController()->getInvokeArg('bootstrap')
-			->getResource('logger')->log($message, $priority);
+        $logger = $this->getLogger();
+        if (null !== $message)
+        {
+            $logger->log($message, $priority);
+        }
+        return $logger;
 	}
+
+    /**
+     * Returns the logger
+     *
+     * @return Zend_Log
+     */
+    public function getLogger()
+    {
+        if (null !== $this->_logger)
+        {
+            return $this->_logger;
+        }
+        
+        $bootstrap = $this->getActionController()
+            ->getInvokeArg('bootstrap');
+
+        if ($bootstrap->hasResource('log'))
+        {
+            return $bootstrap->getResource('log');
+        }
+
+        /**
+         * @see Zend_Log
+         */
+        require_once 'Zend/Log.php';
+        $this->_logger = new Zend_Log(new Zend_Log_Writer_Null());
+
+        return $this->_logger;
+    }
 }
