acbe00595f60db4832a9755f8b163cdd8adf0b31
cizar <cesarkastli@gmail.com>
2012-07-29 18:45:57 -0300

    Nuevos filtros

---
+++ library/GlassOnion/Filter/UnderscoreCase.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @see Zend_Filter_Interface
+ */
+require_once 'Zend/Filter/Interface.php';
+
+/**
+ * @category   GlassOnion
+ * @package    GlassOnion_Filter
+ */
+class GlassOnion_Filter_UnderscoreCase implements Zend_Filter_Interface
+{
+	/**
+	 * Defined by Zend_Filter_Interface
+	 *
+	 * Converts a camel case string into underscored
+	 *
+	 * @param string $value
+	 * @return string
+	 */
+	public function filter($value)
+	{
+		return strtolower(preg_replace('/([^A-Z])([A-Z])/', '$1_$2', $value)); 
+	}
+}
---
+++ library/GlassOnion/Filter/UpperCamelCase.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @see Zend_Filter_Interface
+ */
+require_once 'Zend/Filter/Interface.php';
+
+/**
+ * @category   GlassOnion
+ * @package    GlassOnion_Filter
+ */
+class GlassOnion_Filter_UpperCamelCase implements Zend_Filter_Interface
+{
+	/**
+	 * Defined by Zend_Filter_Interface
+	 *
+	 * Converts a underscored string into UpperCamelCase
+	 *
+	 * @param string $value
+	 * @return string
+	 */
+	public function filter($value)
+	{
+		return preg_replace('/(?:^|_)(.?)/e', 'strtoupper("$1")', $value); 
+	}
+}
