04c11c33c1e31b94fef92aa9143f8e1535e53061
cizar <cesarkastli@gmail.com>
2012-08-29 20:44:36 -0300

    View Helper de FOP

---
+++ library/GlassOnion/View/Helper/Fop.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @see Zend_View_Helper_Abstract
+ */
+require_once 'Zend/View/Helper/Abstract.php';
+
+/**
+ * @category   GlassOnion
+ * @package    GlassOnion_View
+ * @subpackage Helper
+ */
+class GlassOnion_View_Helper_Fop extends Zend_View_Helper_Abstract
+{
+    public function fop($xml, $xslFilename, $charset = 'UTF-8', $keepLayouts = false)
+    {
+        $fop = Zend_Controller_Front::getInstance()
+            ->getParam('bootstrap')
+            ->getResource('fop');
+
+        $pdf = $fop->setXslFilename($xslFilename)
+            ->setXml($xml)
+            ->execute()
+            ->getPdf();
+
+        if (!$keepLayouts) {
+            $layout = Zend_Layout::getMvcInstance();
+            if ($layout instanceof Zend_Layout) {
+                $layout->disableLayout();
+            }
+        }
+
+        $contentType = 'application/pdf; charset=' . $charset;
+
+        Zend_Controller_Front::getInstance()->getResponse()
+            ->setHeader('Content-Type', $contentType)
+            ->setHeader('Cache-Control', 'no-cache');
+
+        return $pdf;
+    }
+}
+
