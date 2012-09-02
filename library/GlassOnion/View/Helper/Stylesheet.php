9737a79bbc7d4cbdd3793d1a5c6564d2b5e8260d
cizar <cesarkastli@gmail.com>
2012-09-01 23:21:23 -0300

    FIX

--- library/GlassOnion/View/Helper/Stylesheet.php
+++ library/GlassOnion/View/Helper/Stylesheet.php
@@ -13,9 +13,16 @@ require_once 'Zend/View/Helper/Abstract.php';
 class GlassOnion_View_Helper_Stylesheet
     extends Zend_View_Helper_Abstract
 {
+    /**
+     * Defined by Zend_Application_Resource_Resource
+     *
+     * @param string $filename
+     * @return GlassOnion_View_Helper_Stylesheet
+     */
     public function stylesheet($filename = null)
     {
-        return $this->view->headLink()->appendStylesheet(
+        $this->view->headLink()->appendStylesheet(
             $this->view->themeBaseUrl($filename));
+        return $this;
     }
 }
