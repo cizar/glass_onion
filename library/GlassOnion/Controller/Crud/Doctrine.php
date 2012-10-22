519c86de06e89332527e66b5897e6eede50d5062
cizar <cesarkastli@gmail.com>
2012-10-22 19:51:46 -0300

    Fix CRUD

--- library/GlassOnion/Controller/Crud/Doctrine.php
+++ library/GlassOnion/Controller/Crud/Doctrine.php
@@ -17,14 +17,14 @@ abstract class GlassOnion_Controller_Crud_Doctrine
      *
      * @var string
      */
-    private $_modelClass = null;
+    private $_tableName = null;
 
     /**
      * The number of items per page
      *
      * @var integer
      */
-    private $_itemCountPerPage = 10;
+    private $_itemCountPerPage = 30;
     
     /**
      * Auto-Redirect when listing only returns one match
@@ -34,18 +34,11 @@ abstract class GlassOnion_Controller_Crud_Doctrine
     private $_oneMatchRedirect = true;
     
     /**
-     * The default sort field
+     * The default sorting data
      *
      * @var string
      */
-    private $_defaultSortField = null;
-
-    /**
-     * The default sort order (asc or desc)
-     *
-     * @var string
-     */
-    private $_defaultSortOrder = 'asc';
+    private $_sortDefault = null;
 
     /**
      * @return void
@@ -62,7 +55,7 @@ abstract class GlassOnion_Controller_Crud_Doctrine
             new GlassOnion_Paginator_Adapter_DoctrineQuery($query));
 
         $paginator->setItemCountPerPage($this->_itemCountPerPage)
-            ->setCurrentPageNumber($this->_getParam('page', 1));
+                  ->setCurrentPageNumber($this->_getParam('page', 1));
 
         if (
             $this->_oneMatchRedirect
@@ -97,44 +90,23 @@ abstract class GlassOnion_Controller_Crud_Doctrine
      */
     protected function sortIndexQuery(Doctrine_Query $query)
     {
-        if ($data = $this->_getSortData()) {
-            list($field, $order) = $data;
+        $pattern = '/(asc|desc)ending_by_([a-z_]+)/';
+        if (preg_match($pattern, $this->_getParam('sort', $this->_sortDefault), $matches)) {
+            list($field, $order) = array($matches[2], $matches[1]);
             $query->orderBy("$field $order");
-            $this->view->sortData = array('order' => $order, 'field' => $field);
+            $this->view->sortData = array('field' => $field, 'order' => $order);
         }
     }
     
-    /**
-     * @return array
-     */
-    private function _getSortData()
-    {
-        $pattern = '/(asc|desc)ending_by_([a-z_]+)/';
-        if (preg_match($pattern, $this->_getParam('sort'), $matches)) {
-            return array($matches[2], $matches[1]);
-        }
-        if ($this->_defaultSortField) {
-            return array($this->_defaultSortField, $this->_defaultSortOrder);
-        }
-        return null;
-    }
-
     /**
      * @return Doctrine_Query
      */
     protected function getIndexQuery()
     {
-        $this->_assertModelIsDefined();
-        
-        $query = Doctrine_Query::create()
-            ->from($this->_modelClass);
-            
+        $query = $this->getTable()->createQuery();
         $this->prepareIndexQuery($query);
-
         $this->filerIndexQuery($query);
-
         $this->sortIndexQuery($query);
-
         return $query;
     } 
 
@@ -171,15 +143,7 @@ abstract class GlassOnion_Controller_Crud_Doctrine
         $this->record = $record;
         $this->view->record = $record;
     }
-    
-    /**
-     * Create
-     */
-    protected function create(Doctrine_Record $record)
-    {
-        $record->fromArray($this->_getParam('record'));
-    }
-    
+        
     /**
      * @return void
      */
@@ -203,6 +167,24 @@ abstract class GlassOnion_Controller_Crud_Doctrine
         $this->record = $record;
         $this->view->record = $record;
     }
+    
+    /**
+     * @return void
+     */
+    public function deleteAction()
+    {
+        $record = $this->getRecord($this->_getParam('id'));
+        $this->destroy($record);
+        $this->_helper->redirector('index');
+    }
+
+    /**
+     * Create
+     */
+    protected function create(Doctrine_Record $record)
+    {
+        $record->fromArray($this->_getParam('record'));
+    }
 
     /**
      * Update
@@ -211,21 +193,13 @@ abstract class GlassOnion_Controller_Crud_Doctrine
     {
         $record->fromArray($this->_getParam('record'));
     }
-    
+
     /**
-     * @return void
+     * Destroy
      */
-    public function deleteAction()
+    protected function destroy(Doctrine_Record $record)
     {
-        $record = $this->getRecord($this->_getParam('id'));
-
         $record->delete();
-
-        if (method_exists($this, 'destroy')) {
-            $this->destroy($record);
-        }
-
-        $this->_helper->redirector('index');
     }
 
     /**
@@ -233,19 +207,17 @@ abstract class GlassOnion_Controller_Crud_Doctrine
      *
      * @return GlassOnion_Controller_Crud_Doctrine Provides a fluent interface
      */
-    protected function useModel($class)
+    protected function useModel($tableName)
     {
-        if (!class_exists($class)) {
+        if (!class_exists($tableName)) {
             /**
              * @see GlassOnion_Controller_Crud_Exception
              */
             require_once 'GlassOnion/Controller/Crud/Exception.php';
             throw new GlassOnion_Controller_Crud_Exception(
-                'The model ' . $class . ' does not exists');
+                'The class ' . $tableName . ' does not exists');
         }
-        
-        $this->_modelClass = $class;
-        
+        $this->_tableName = $tableName;
         return $this;
     }
     
@@ -295,33 +267,10 @@ abstract class GlassOnion_Controller_Crud_Doctrine
      *
      * @return GlassOnion_Controller_Crud_Doctrine Provides a fluent interface
      */
-    public function sortDefaults($field, $order = null)
+    public function sortDefaults($field, $order = 'asc')
     {
-        $this->_defaultSortField = $field;
-        if (null !== $order) {
-            $this->_defaultSortOrder = $order;
-        }
+        $this->_sortDefault = $order . 'ending_by_' . $field;
         return $this;
-    }
-    
-    /**
-     * Find a record or create a new one if not exists. Either case return the record.
-     *
-     * @param  string $tableName
-     * @param  string $fieldName
-     * @param  string $value
-     * @param  bool $createIfNotExists
-     * @return void
-     * @throws Zend_Controller_Action_Exception
-     */
-    public function getRecordBy($tableName, $fieldName, $value, $createIfNotExists = false)
-    {
-        $record = Doctrine_Core::getTable($tableName)->findOneBy($fieldName, $value);
-        if (!$record && $createIfNotExists) {
-            $record = new $tableName;
-            $record->set($fieldName, $value);
-        }
-        return $record;
     }
     
     /**
@@ -335,21 +284,33 @@ abstract class GlassOnion_Controller_Crud_Doctrine
     public function __call($methodName, $args)
     {
         $pattern = '/(getOrCreate|get)([a-z_]+)By([a-z_]+)/i';
-        
         if (!preg_match($pattern, $methodName, $matches)) {
             return parent::__call($methodName, $args);
         }
-        
-        if (1 !== count($args)) {
-            require_once 'GlassOnion/Controller/Crud/Exception.php';
-            throw new GlassOnion_Controller_Crud_Exception(
-                sprintf('%s requires one and only one argument', $methodName));
-        }
-        
         require_once 'Doctrine/Inflector.php';
         $fieldName = Doctrine_Inflector::tableize($matches[3]);
-        
-        return $this->getRecordBy($matches[2], $fieldName, $args[0], 'getOrCreate' === $matches[1]);
+        return $this->getRecordBy($fieldName, $args, 'getOrCreate' === $matches[1], $matches[2]);
+    }
+    
+    /**
+     * Find a record or create a new one if not exists. Either case return the record.
+     *
+     * @param  string $fieldName
+     * @param  string $value
+     * @param  bool $createIfNotExists
+     * @param  string $tableName
+     * @return void
+     * @throws Zend_Controller_Action_Exception
+     */
+    protected function getRecordBy($fieldName, $value, $createIfNotExists = false, $tableName = null)
+    {
+        $table = $this->getTable($tableName);
+        $record = $table->findOneBy($fieldName, $value);
+        if (!$record && $createIfNotExists) {
+            $record = $table->create();
+            $record->set($fieldName, $value);
+        }
+        return $record;
     }
 
     /**
@@ -357,13 +318,18 @@ abstract class GlassOnion_Controller_Crud_Doctrine
      *
      * @return Doctrine_Record
      */
-    protected function getRecord($id = null)
+    protected function getRecord($id, $tableName = null)
     {
-        if (is_null($id)) {
-            return $this->getNewRecord();
+        $record = $this->getTable($tableName)->find($id);
+        if (!$record) {
+            /**
+             * @see GlassOnion_Controller_Crud_Exception
+             */
+            require_once 'GlassOnion/Controller/Crud/Exception.php';
+            throw new GlassOnion_Controller_Crud_Exception(
+                'The record does not exists');
         }
-
-        return $this->getTable()->find($id);
+        return $record;
     }
 
     /**
@@ -371,22 +337,9 @@ abstract class GlassOnion_Controller_Crud_Doctrine
      *
      * @return Doctrine_Record
      */
-    protected function getNewRecord()
+    protected function getNewRecord($tableName = null)
     {
-        $this->_assertModelIsDefined();
-        
-        $record = new $this->_modelClass;
-
-        if (!is_subclass_of($record, 'Doctrine_Record')) {
-            /**
-             * @see GlassOnion_Controller_Crud_Exception
-             */
-            require_once 'GlassOnion/Controller/Crud/Exception.php';
-            throw new GlassOnion_Controller_Crud_Exception(
-                'The object is not a Doctrine_Record');
-        }
-
-        return $record;
+        return $this->getTable($tableName)->create();
     }
 
     /**
@@ -394,27 +347,12 @@ abstract class GlassOnion_Controller_Crud_Doctrine
      *
      * @return Doctrine_Table
      */
-    protected function getTable()
+    protected function getTable($tableName = null)
     {
-        $this->_assertModelIsDefined();
-        return Doctrine_Core::getTable($this->_modelClass);   
-    }
-
-    /**
-     * Verify that the assumption of the existence of the method is correct
-     *
-     * @return void
-     */
-    private function _assertModelIsDefined()
-    {
-        if (null === $this->_modelClass) {
-            /**
-             * @see GlassOnion_Controller_Crud_Exception
-             */
-            require_once 'GlassOnion/Controller/Crud/Exception.php';
-            throw new GlassOnion_Controller_Crud_Exception(
-                'No model has been defined');
+        if (null === $tableName) {
+            $tableName = $this->_tableName;
         }
+        return Doctrine_Core::getTable($tableName);   
     }
     
     /**
