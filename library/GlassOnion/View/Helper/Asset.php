975c673b34f05a1c3cfc8994449bee6e093cd222
Cesar <cesar@virtual.(none)>
2013-01-04 16:03:35 -0300

    Se aceptan varios assets en la misma llamada

--- library/GlassOnion/View/Helper/Asset.php
+++ library/GlassOnion/View/Helper/Asset.php
@@ -16,28 +16,32 @@ class GlassOnion_View_Helper_Asset
     /**
      * Appends an asset and all its dependencies
      *
-     * @param string $href
+     * @param string $id
      * @return Zend_View
      */
-    public function asset($id)
+    public function asset()
     {
+        $ids = func_get_args();
+
         $assets = Zend_Controller_Front::getInstance()
             ->getParam('bootstrap')
             ->getResource('assets');
         
-        foreach ($assets->getRequiredAssets($id) as $asset) {
-            switch ($asset->getClass()) {
-                case 'script':
-                    $this->view->script($asset->src,
-                        $asset->getProperty('type', 'text/javascript'));
-                    break;
-                case 'stylesheet':
-                    $this->view->stylesheet($asset->href,
-                        $asset->getProperty('media', 'screen'));
-                    break;
-                case 'favicon':
-                    $this->view->favicon($asset->href);
-                    break;
+        foreach ($ids as $id) {
+            foreach ($assets->getRequiredAssets($id) as $asset) {
+                switch ($asset->getClass()) {
+                    case 'script':
+                        $this->view->script($asset->src,
+                            $asset->getProperty('type', 'text/javascript'));
+                        break;
+                    case 'stylesheet':
+                        $this->view->stylesheet($asset->href,
+                            $asset->getProperty('media', 'screen'));
+                        break;
+                    case 'favicon':
+                        $this->view->favicon($asset->href);
+                        break;
+                }
             }
         }
 
