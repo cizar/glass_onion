0f15465053c764018b5b9d98bdb054ff91b2c6c7
Cesar Kastli <cesarkastli@gmail.com>
2014-03-13 11:09:10 -0300

    Nuevos helpers para Bootstrap

---
+++ library/GlassOnion/View/Helper/BootstrapFormDate.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Glass Onion
+ *
+ * Copyright (c) 2009 César Kästli (cesarkastli@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any
+ * person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of
+ * the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @copyright  Copyright (c) 2009 César Kästli (cesarkastli@gmail.com)
+ * @license    MIT
+ */
+
+/**
+ * @see GlassOnion_View_Helper_FormDate
+ */
+require_once 'GlassOnion/View/Helper/FormDate.php';
+
+/**
+ * @category   GlassOnion
+ * @package    GlassOnion_View
+ * @subpackage Helper
+ */
+class GlassOnion_View_Helper_BootstrapFormDate extends GlassOnion_View_Helper_FormDate
+{
+	public function bootstrapFormDate($name, $value = null, $params = null, $attribs = null)
+	{
+		if (is_array($attribs)) {
+			if (!array_key_exists('class', $attribs)) {
+				$attribs['class'] = 'form-control';
+			}
+		} else {
+			$attribs = array('class' => 'form-control');
+		}
+		return $this->formDate($name, $value, $params, $attribs);
+	}
+}
