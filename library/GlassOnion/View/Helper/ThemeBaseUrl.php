fcb356227eff168af781a9ce96ac3fa5b83b2c0b
Cesar Kastli <cesarkastli@gmail.com>
2015-09-18 11:31:52 -0300

    The helper now returns it instance

--- library/GlassOnion/View/Helper/ThemeBaseUrl.php
+++ library/GlassOnion/View/Helper/ThemeBaseUrl.php
@@ -13,11 +13,11 @@
  * distribute, sublicense, and/or sell copies of the
  * Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice
  * shall be included in all copies or substantial portions of
  * the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
  * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
  * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
@@ -42,87 +42,103 @@ require_once 'Zend/View/Helper/Abstract.php';
  * @subpackage Helper
  */
 class GlassOnion_View_Helper_ThemeBaseUrl
-    extends Zend_View_Helper_Abstract
+  extends Zend_View_Helper_Abstract
 {
-    /**
-     * @const string
-     */
-    const DEFAULT_THEME = 'default';
+  /**
+   * @const string
+   */
+  const DEFAULT_THEME = 'default';
 
-    /**
-     * @const string
-     */
-    const DEFAULT_BASE_URL = '/themes';
+  /**
+   * @const string
+   */
+  const DEFAULT_BASE_URL = '/themes';
 
-    /**
-     * @var string
-     */
-    protected $_theme;
-    
-    /**
-     * @var string
-     */
-    protected $_baseUrl;
+  /**
+   * @var string
+   */
+  protected $_theme;
 
-    /**
-     * Returns the theme's base url or file with the base url prepended
-     *
-     * @param string $theme
-     * @return string
-     */
-    public function themeBaseUrl($file = null)
-    {
-        $path = $this->getBaseUrl() . '/' . $this->getTheme() . '/' . $file;
-        return $this->view->baseUrl($path);
+  /**
+   * @var string
+   */
+  protected $_baseUrl;
+
+  /**
+   * @var string
+   */
+  protected $_path;
+
+  /**
+   * Returns the theme's base url or file with the base url prepended
+   *
+   * @param string $theme
+   * @return string
+   */
+  public function themeBaseUrl($path = null)
+  {
+    $this->_path = $path;
+    return $this;
+  }
+
+  /**
+   * Set the theme name
+   *
+   * @param string $theme
+   * @return GlassOnion_View_Helper_ThemeBaseUrl Provides a fluent interface
+   */
+  public function setTheme($theme)
+  {
+    $this->_theme = $theme;
+    return $this;
+  }
+
+  /**
+   * Returns the theme name
+   *
+   * @return string
+   */
+  public function getTheme()
+  {
+    if (null === $this->_theme) {
+      $this->_theme = self::DEFAULT_THEME;
     }
+    return $this->_theme;
+  }
 
-    /**
-     * Set the theme name
-     *
-     * @param string $theme
-     * @return GlassOnion_View_Helper_ThemeBaseUrl Provides a fluent interface
-     */
-    public function setTheme($theme)
-    {
-        $this->_theme = $theme;
-        return $this;
-    }
+  /**
+   * Set the theme base URL
+   *
+   * @param string $baseUrl
+   * @return GlassOnion_View_Helper_ThemeBaseUrl Provides a fluent interface
+   */
+  public function setBaseUrl($baseUrl)
+  {
+    $this->_baseUrl = rtrim($baseUrl, '/\\');
+    return $this;
+  }
 
-    /**
-     * Returns the theme name
-     *
-     * @return string
-     */
-    public function getTheme()
-    {
-        if (null === $this->_theme) {
-            $this->_theme = self::DEFAULT_THEME;
-        }
-        return $this->_theme;
+  /**
+   * Returns the theme base URL
+   *
+   * @return string
+   */
+  public function getBaseUrl()
+  {
+    if (null === $this->_baseUrl) {
+      $this->_baseUrl = self::DEFAULT_BASE_URL;
     }
+    return $this->_baseUrl;
+  }
 
-    /**
-     * Set the theme base URL
-     *
-     * @param string $baseUrl
-     * @return GlassOnion_View_Helper_ThemeBaseUrl Provides a fluent interface
-     */
-    public function setBaseUrl($baseUrl)
-    {
-        $this->_baseUrl = rtrim($baseUrl, '/\\');
-        return $this;
-    }
-
-    /**
-     * Returns the theme base URL
-     *
-     * @return string
-     */
-    public function getBaseUrl()
-    {
-        if (null === $this->_baseUrl) {
-            $this->_baseUrl = self::DEFAULT_BASE_URL;
-        }
-        return $this->_baseUrl;
-    }
+  /**
+  * Cast to string
+   *
+   * @return string
+   */
+  public function __toString()
+  {
+    $path = $this->getBaseUrl() . '/' . $this->getTheme() . '/' . $this->_path;
+    return $this->view->baseUrl($path);
+  }
 }
