49917b3d8c81c549f78c961de7fa4b0528f20124
Cesar Kastli <cesarkastli@gmail.com>
2015-10-31 15:48:53 -0300

    New action helper

---
+++ library/GlassOnion/Controller/Action/Helper/AjaxLayout.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Glass Onion
+ *
+ * Copyright (c) 2009 César Kästli (cesarkastli@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any
+ * person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @copyright  Copyright (c) 2009 César Kästli (cesarkastli@gmail.com)
+ * @license    MIT
+ */
+
+/**
+ * @see Zend_Controller_Action_Helper_Abstract
+ */
+require_once 'Zend/Controller/Action/Helper/Abstract.php';
+
+/**
+ * @category   GlassOnion
+ * @package    GlassOnion_Controller
+ * @subpackage Helper
+ */
+class GlassOnion_Controller_Action_Helper_AjaxLayout
+  extends Zend_Controller_Action_Helper_Abstract
+{
+  public function direct()
+  {
+    $request = $this->getRequest();
+    $helper = Zend_Controller_Action_HelperBroker::getStaticHelper('layout');
+
+    if ($request->isXmlHttpRequest()) {
+      $layout = $request->getHeader('X-Layout');
+      if ($layout) {
+        $helper->setLayout($layout);
+      } else {
+        $helper->disableLayout();
+      }
+    }
+  }
+}
